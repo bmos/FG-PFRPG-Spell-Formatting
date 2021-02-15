30d356d874ed96d0ef84fb1bce39780fa02cd7c1
Wil Thieme <wil.thieme@protonmail.com>
2021-02-15 15:14:22 -0500

    remove old code and rename manager_spell2.lua

---
+++ scripts/spell_formatting.lua
@@ -0,0 +1,35 @@
+-- 
+-- Please see the LICENSE.md file included with this distribution for attribution and copyright information.
+--
+
+-- local convertSpellDescToString_old = nil
+local addSpell_old = nil
+
+---	This function copies the fully-formatted text into newly-created spells
+local function addSpell_new(nodeSource, nodeSpellClass, nLevel)
+	-- Validate
+	if not nodeSource or not nodeSpellClass or not nLevel then
+		return nil;
+	end
+	
+	-- Get the new spell entry
+	local nodeNewSpell = addSpell_old(nodeSource, nodeSpellClass, nLevel)
+	if not nodeNewSpell then
+		return nil;
+	end
+	
+	-- Copy the formatted spell details over
+	DB.setValue(nodeNewSpell, 'description_full', 'formattedtext', DB.getValue(nodeSource, 'description', '<p></p>'));
+	
+	return nodeNewSpell;
+end
+
+-- Function Overrides
+function onInit()
+	addSpell_old = SpellManager.addSpell;
+	SpellManager.addSpell = addSpell_new;
+end
+
+function onClose()
+	SpellManager.addSpell = addSpell_old;
+end
